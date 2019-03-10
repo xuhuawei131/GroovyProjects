b9223996c9cde9e37b468e3b089687a3a9e92e09
xuhuawei <xuhw@knowbox.cn>
2019-03-11 00:38:21 +0800

    添加字符串

--- GroovyDemo03/src/strings/stringstudy.groovy
+++ GroovyDemo03/src/strings/stringstudy.groovy
@@ -29,7 +29,36 @@ def sum="the sum of 2 and 3 equals ${2+3}"
 //println(sum)
 
 def result=echo(sum)
-println(result.class)
+//println(result.class)
 String echo(String message){
     return message
 }
+/*=================================字符串的方法====================================*/
+//字符串填充
+def str="groovy"
+println str.center(8,"*")
+println str.center(8)//默认是填充空格
+println str.padLeft(8,"*")//填充在已有字符串的左边，padRight正好相反
+
+//字符串比较
+def str2="Hello"
+println str.compareTo(str2)//使用方法比较
+println str>str2 //使用操作符比较 ；除了数字可以进行比较之外，字符串我们也可以比较
+
+//字符串索引 获取字符串中某个字符
+println str.getAt(0)
+println str[0]//使用类似于数组下标的方式
+println str[0..1]//除了单个下标 还可以使用范围
+
+//字符串减法
+println str.minus(str2)//从str字符串中 减去str2相同的部分  由于他们没有相同的 所以原样输出str
+
+def str3="Groovy Hello"
+println str3.minus(str2)//从str3字符串中 减去str2相同的部分  减去相同的部分Hello 所以还剩下Groovy
+
+
+//other 没有什么相关的了
+println str.reverse()//字符串倒序
+println(str.capitalize())//首字母大写 内部其实是找到第一个字母 变成大写之后返回新的字符串
+println str.isNumber() //字符串是否是数字类型的
+println str.toLong() //转化成Long类型 进行类型转换
